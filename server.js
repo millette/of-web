998b1637ddb950b8aa46b525bae4ccc3a45c3fcc
Robin Millette <robin@millette.info>
2018-11-24 22:25:02 -0500

    Golfing.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // npm
-const fastify = require("fastify")({ logger: { level: "error" } })
+const { register, listen } = require("fastify")({ logger: { level: "error" } })
 const nextjs = require("next")
 
 // self
@@ -8,16 +8,20 @@ const mabo = require("./data/mabo.json")
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== "production"
 
-fastify.register((fastify, opts, next) => {
+register((fastify, opts, next) => {
+  const get = fastify.get.bind(fastify)
+  const setNotFoundHandler = fastify.setNotFoundHandler.bind(fastify)
   const app = nextjs({ dev })
   const handleRequest = app.handleRequest.bind(app)
   const render = app.render.bind(app)
   const render404 = app.render404.bind(app)
   const prepare = app.prepare.bind(app)
+
   const handler = (req, reply) =>
     handleRequest(req.req, reply.res).then(() => {
       reply.sent = true
     })
+
   const send404 = (req, reply) =>
     render404(req.req, reply.res).then(() => {
       reply.sent = true
@@ -26,27 +30,27 @@ fastify.register((fastify, opts, next) => {
   prepare()
     .then(() => {
       if (dev) {
-        fastify.get("/_next/*", handler)
-        fastify.get("/_error/*", handler)
+        get("/_next/*", handler)
+        get("/_error/*", handler)
       }
 
       /*
-    fastify.get('/a', (req, reply) => {
-      return render(req.req, reply.res, '/b', req.query)
-        .then(() => {
-          reply.sent = true
-        })
-    })
+      get('/a', (req, reply) => {
+        return render(req.req, reply.res, '/b', req.query)
+          .then(() => {
+            reply.sent = true
+          })
+      })
 
-    fastify.get('/b', (req, reply) => {
-      return render(req.req, reply.res, '/a', req.query)
-        .then(() => {
-          reply.sent = true
-        })
-    })
-    */
+      get('/b', (req, reply) => {
+        return render(req.req, reply.res, '/a', req.query)
+          .then(() => {
+            reply.sent = true
+          })
+      })
+      */
 
-      fastify.get("/api/mabo", (req, reply) => {
+      get("/api/mabo", (req, reply) => {
         if (!req.query.q) {
           reply.type("application/json")
           return reply.send(mabo)
@@ -58,19 +62,14 @@ fastify.register((fastify, opts, next) => {
         return send404(req, reply)
       })
 
-      fastify.get("/*", handler)
-      fastify.setNotFoundHandler(send404)
+      get("/*", handler)
+      setNotFoundHandler(send404)
       next()
     })
     .catch(next)
-  /*
-  } catch (e) {
-    next(e)
-  }
-  */
 })
 
-fastify.listen(port, (err) => {
+listen(port, (err) => {
   if (err) throw err
   console.log(`> Ready on http://localhost:${port}`)
 })
