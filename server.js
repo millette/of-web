ef8ed1c2dbe5e468b6325e592aa185f8ca657bb6
Robin Millette <robin@millette.info>
2018-11-24 21:46:57 -0500

    Server bugfix.

--- server.js
+++ server.js
@@ -8,71 +8,76 @@ const mabo = require("./data/mabo.json")
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== "production"
 
-fastify.register((fastify, opts, next) => {
+fastify.register(async (fastify, opts, next) => {
   const app = nextjs({ dev })
 
-  const { handleRequest, render, render404 } = app
-  app
-    .prepare()
-    .then(() => {
-      if (dev) {
-        fastify.get("/_next/*", (req, reply) => {
-          return handleRequest(req.req, reply.res).then(() => {
-            reply.sent = true
-          })
-        })
+  const handleRequest = app.handleRequest.bind(app)
+  const render = app.render.bind(app)
+  const render404 = app.render404.bind(app)
+  const prepare = app.prepare.bind(app)
 
-        fastify.get("/_error/*", (req, reply) => {
-          return handleRequest(req.req, reply.res).then(() => {
-            reply.sent = true
-          })
-        })
-      }
-
-      /*
-      fastify.get('/a', (req, reply) => {
-        return render(req.req, reply.res, '/b', req.query)
-          .then(() => {
-            reply.sent = true
-          })
-      })
-
-      fastify.get('/b', (req, reply) => {
-        return render(req.req, reply.res, '/a', req.query)
-          .then(() => {
-            reply.sent = true
-          })
-      })
-      */
-
-      fastify.get("/api/mabo", async (req, reply) => {
-        if (req.query.q) {
-          if (mabo[0].products[req.query.q]) {
-            reply.type("application/json")
-            reply.send(mabo[0].products[req.query.q])
-          } else {
-            await render404(req.req, reply.res)
-            reply.sent = true
-          }
-          return
-        }
-        reply.type("application/json")
-        reply.send(mabo)
-      })
-
-      fastify.get("/*", async (req, reply) => {
+  try {
+    await prepare()
+    //.then(() => {
+    if (dev) {
+      fastify.get("/_next/*", async (req, reply) => {
         await handleRequest(req.req, reply.res)
         reply.sent = true
       })
 
-      fastify.setNotFoundHandler(async (request, reply) => {
-        await render404(request.req, reply.res)
+      fastify.get("/_error/*", async (req, reply) => {
+        await handleRequest(req.req, reply.res)
         reply.sent = true
       })
+    }
 
-      next()
+    /*
+    fastify.get('/a', (req, reply) => {
+      return render(req.req, reply.res, '/b', req.query)
+        .then(() => {
+          reply.sent = true
+        })
     })
-    .catch(next)
+
+    fastify.get('/b', (req, reply) => {
+      return render(req.req, reply.res, '/a', req.query)
+        .then(() => {
+          reply.sent = true
+        })
+    })
+    */
+
+    fastify.get("/api/mabo", async (req, reply) => {
+      if (req.query.q) {
+        if (mabo[0].products[req.query.q]) {
+          reply.type("application/json")
+          reply.send(mabo[0].products[req.query.q])
+        } else {
+          await render404(req.req, reply.res)
+          reply.sent = true
+        }
+        return
+      }
+      reply.type("application/json")
+      reply.send(mabo)
+    })
+
+    fastify.get("/*", async (req, reply) => {
+      await handleRequest(req.req, reply.res)
+      reply.sent = true
+    })
+
+    fastify.setNotFoundHandler(async (request, reply) => {
+      await render404(request.req, reply.res)
+      reply.sent = true
+    })
+
+    next()
+    // })
+    // .catch(next)
+  } catch (e) {
+    next(e)
+  }
 })
 
 fastify.listen(port, (err) => {
