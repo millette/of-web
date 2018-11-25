4eb7b9bd974ff670b483044955b30e937d518ab4
Robin Millette <robin@millette.info>
2018-11-24 22:11:55 -0500

    Remove async/await.

--- server.js
+++ server.js
@@ -8,27 +8,29 @@ const mabo = require("./data/mabo.json")
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== "production"
 
-fastify.register(async (fastify, opts, next) => {
+fastify.register((fastify, opts, next) => {
   const app = nextjs({ dev })
-
   const handleRequest = app.handleRequest.bind(app)
   const render = app.render.bind(app)
   const render404 = app.render404.bind(app)
   const prepare = app.prepare.bind(app)
-
   const handler = (req, reply) =>
     handleRequest(req.req, reply.res).then(() => {
       reply.sent = true
     })
+  const send404 = (req, reply) =>
+    render404(req.req, reply.res).then(() => {
+      reply.sent = true
+    })
 
-  try {
-    await prepare()
-    if (dev) {
-      fastify.get("/_next/*", handler)
-      fastify.get("/_error/*", handler)
-    }
+  prepare()
+    .then(() => {
+      if (dev) {
+        fastify.get("/_next/*", handler)
+        fastify.get("/_error/*", handler)
+      }
 
-    /*
+      /*
     fastify.get('/a', (req, reply) => {
       return render(req.req, reply.res, '/b', req.query)
         .then(() => {
@@ -44,32 +46,28 @@ fastify.register(async (fastify, opts, next) => {
     })
     */
 
-    fastify.get("/api/mabo", async (req, reply) => {
-      if (req.query.q) {
+      fastify.get("/api/mabo", (req, reply) => {
+        if (!req.query.q) {
+          reply.type("application/json")
+          return reply.send(mabo)
+        }
         if (mabo[0].products[req.query.q]) {
           reply.type("application/json")
-          reply.send(mabo[0].products[req.query.q])
-        } else {
-          await render404(req.req, reply.res)
-          reply.sent = true
+          return reply.send(mabo[0].products[req.query.q])
         }
-        return
-      }
-      reply.type("application/json")
-      reply.send(mabo)
+        return send404(req, reply)
+      })
+
+      fastify.get("/*", handler)
+      fastify.setNotFoundHandler(send404)
+      next()
     })
-
-    fastify.get("/*", handler)
-
-    fastify.setNotFoundHandler(async (req, reply) => {
-      await render404(req.req, reply.res)
-      reply.sent = true
-    })
-
-    next()
+    .catch(next)
+  /*
   } catch (e) {
     next(e)
   }
+  */
 })
 
 fastify.listen(port, (err) => {
