58b84e148858342f64e97aca55ce74b3e59397d4
Robin Millette <robin@millette.info>
2018-11-26 23:09:56 -0500

    Add viewport and html lang attribute.

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Main, NextScript } from "next/document"
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    let [, lang] = ctx.req.url.split("/")
+    if (lang !== "en") lang = "fr"
+    return { ...initialProps, lang }
+  }
+
+  render() {
+    return (
+      <html lang={this.props.lang}>
+        <Head>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width, shrink-to-fit=no"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
