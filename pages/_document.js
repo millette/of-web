177f5d319884161f3513c8aae9d39c8e4dd9eafc
Robin Millette <robin@millette.info>
2018-11-27 04:44:59 -0500

    Load css in main html.

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
             name="viewport"
             content="initial-scale=1.0, width=device-width, shrink-to-fit=no"
           />
+          <link rel="stylesheet" href="/bulma.css" />
         </Head>
         <body>
           <Main />
