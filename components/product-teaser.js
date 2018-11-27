01b333114a6e21216a86c034acccb001fa626b31
Robin Millette <robin@millette.info>
2018-11-27 06:00:20 -0500

    Improve style a bit.

--- components/product-teaser.js
+++ components/product-teaser.js
@@ -5,28 +5,32 @@ export default ({ className, product: { url, microdata }, i }) => {
   const { name, description, image } = microdata["@graph"][0]
   return (
     <div className={className}>
-      <h3 className="title is-5">
-        {i >= 0 ? (
-          <Link prefetch href={`/item?q=${i}`} as={`/item/${i}`}>
-            <a>{name}</a>
-          </Link>
-        ) : (
-          name
-        )}
-      </h3>
-      <div className="columns">
-        <div className="column">
-          <p>{description}</p>
-        </div>
-        {!(i >= 0) && (
-          <div className="column">
-            <figure className="image">
-              <img src={image} />
-            </figure>
+      <div className={i >= 0 ? "box" : ""}>
+        <h3 className="title is-5">
+          {i >= 0 ? (
+            <Link prefetch href={`/item?q=${i}`} as={`/item/${i}`}>
+              <a>{name}</a>
+            </Link>
+          ) : (
+            name
+          )}
+        </h3>
+        <div className="columns">
+          <div className="column content">
+            <p>{description}</p>
           </div>
-        )}
+          {!(i >= 0) && (
+            <div className="column">
+              <div className="box">
+                <figure className="image">
+                  <img src={image} />
+                </figure>
+              </div>
+            </div>
+          )}
+        </div>
+        <p>{url}</p>
       </div>
-      <p>{url}</p>
     </div>
   )
 }
