cbd65e114ae30b6c67693aafba9035d9479badc0
Robin Millette <robin@millette.info>
2018-11-28 17:09:26 -0500

    Upgrade next (canary).

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== "production"
 
 if (dev) {
-  decorateReply("etag", function() {})
+  decorateReply("etag", () => false)
 } else {
   register(require("fastify-compress"), {
     customTypes: /.*/,
